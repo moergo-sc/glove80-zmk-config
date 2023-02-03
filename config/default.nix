99a8f96e92b94f465ec67517dee0e57a26fcfc57
Chris Andreae <chris@andreae.gen.nz>
2023-02-03 15:49:52 +0900

    Ensure the whole config directory is present in the nix store
    In order that relative #includes from the keymap work.

--- config/default.nix
+++ config/default.nix
@@ -2,8 +2,9 @@
 
 let
   firmware = import ../src {};
+  config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = ./glove80.keymap; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = ./glove80.keymap; };
+  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; };
+  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; };
 
 in firmware.combine_uf2 glove80_left glove80_right
