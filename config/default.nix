4ad3fd674e981b84d2af9a218802e22647e625c9
Chris Andreae <chris@andreae.gen.nz>
2023-05-19 21:09:46 +0900

    Add support for overriding kconfig configuration
    Passes the provided `glove80.conf` through to the ZMK build

--- config/default.nix
+++ config/default.nix
@@ -4,7 +4,7 @@ let
   firmware = import ../src {};
   config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; };
+  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
 
 in firmware.combine_uf2 glove80_left glove80_right
